99c430f29c3b9eab8b99a027f306cc61c34b44e8
Đoàn Nguyễn Nam <nam137hb@gmail.com>
2025-11-06 11:16:27 +0700

    Working

--- Assets/RW/Scripts/MouseController.cs
+++ Assets/RW/Scripts/MouseController.cs
@@ -1,11 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
-using System.Numerics;
 using UnityEngine;
 
 public class MouseController : MonoBehaviour
 {
     public float jetpackForce = 75.0f;
+    public float forwardMovementSpeed = 3.0f;
+
     private Rigidbody2D playerRigidbody;
 
     void Start()
@@ -18,7 +19,12 @@ public class MouseController : MonoBehaviour
         bool jetpackActive = Input.GetButton("Fire1");
         if (jetpackActive)
         {
-            playerRigidbody.AddForce(new UnityEngine.Vector2(0, jetpackForce));
+            playerRigidbody.AddForce(new Vector2(0, jetpackForce));
         }
+
+        Vector2 newVelocity = playerRigidbody.velocity;
+        newVelocity.x = forwardMovementSpeed;
+        playerRigidbody.velocity = newVelocity;
+
     }
 }
